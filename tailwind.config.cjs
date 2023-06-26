478ad30468edcf5d12b537606cd61c07046d1a4c
khawaja-ammar <mammarkh@gmail.com>
2023-06-26 12:50:35 +0500

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      // fontFamily: {},
+      colors: {
+        primarycolor: "#F1F1F1",
+        secondarycolor: "#FCFCFC",
+        accentcolor: "#F97316",
+        "accentcolor-light": "#FDBA74",
+        "primarycolor-dark": "#1F2937",
+        "secondarycolor-dark": "#374151",
+        "accentcolor-alt": "#00FF00",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
