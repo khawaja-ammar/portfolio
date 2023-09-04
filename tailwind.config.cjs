73fd7cd60b89e126c137f5de771e7cba7228cf48
khawaja-ammar <mammarkh@gmail.com>
2023-09-04 15:22:57 +0500

    removed quoteless made for tailwind typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,14 +23,6 @@ module.exports = {
       animation: {
         "react-logo-spin": "spin infinite 20s linear",
       },
-      typography: {
-        quoteless: {
-          css: {
-            "blockquote p:first-of-type::before": { content: "none" },
-            "blockquote p:first-of-type::after": { content: "none" },
-          },
-        },
-      },
     },
   },
   plugins: [],
