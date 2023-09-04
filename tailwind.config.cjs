78411d90f5d8e3af66b57502bd00ea85eb776ca3
khawaja-ammar <mammarkh@gmail.com>
2023-09-04 12:04:16 +0500

    switched fonts to tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,37 +5,31 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      typography: {
-        quoteless: {
-          css: {
-            'blockquote p:first-of-type::before': { content: 'none' },
-            'blockquote p:first-of-type::after': { content: 'none' },
-          },
-        }
+      fontFamily: {
+        text: ['"Source Sans 3"', '"sans-serif"'],
+        code: ["Fira Code, monospace", { fontVariationSettings: "" }],
       },
-      // fontFamily: {},
       colors: {
         primarycolor: "#F1F1F1",
+        "primarycolor-dark": "#151515",
         secondarycolor: "#FCFCFC",
+        "secondarycolor-dark": "#2B2B2B",
         accentcolor: "#F97316",
         "accentcolor-light": "#FDBA74",
-        "primarycolor-dark": "#151515",
-        // #151515
-        // #1F2937
-        // #141a1f
-        // #262626
-        // #202124
-        "secondarycolor-dark": "#2B2B2B",
-        // #0D1117
-        // #374151
-        // #0d1117
-        // #1e1e1e
         "accentcolor-alt": "#00FF00",
         bordercolor: "black",
         "bordercolor-dark": "black",
       },
       animation: {
         "react-logo-spin": "spin infinite 20s linear",
+      },
+      typography: {
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          },
+        },
       },
     },
   },
