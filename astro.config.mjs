f4caff92a2155c4a25ceda9fb13d6fa53bb3e5f1
khawaja-ammar <mammarkh@gmail.com>
2023-11-26 18:03:50 +0500

    removed cloudflare cli as useless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
+  // output: "server",
+  // adapter: cloudflare(),
   integrations: [tailwind()],
 });
